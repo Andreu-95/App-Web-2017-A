13de8115478115b801012ee2bdb85c3ee49384f9
Andrés Guerra Vásconez <andreu.g.95@gmail.com>
2017-05-22 20:18:02 -0500

    Más funciones

--- 04-Javascript/Funciones/funciones.js
+++ 04-Javascript/Funciones/funciones.js
@@ -6,3 +6,28 @@ function imprimirHolaMundo () {
 }
 
 imprimirHolaMundo();
+
+function imprimir (texto) {
+    console.log(texto);
+}
+
+imprimir("Regreso al vacío...");
+
+function suma2Numeros (a, b) {
+    return a + b;
+}
+
+console.log(suma2Numeros(1, 2));
+
+function operar2Numeros (n1, n2, funcion) {
+    n1 += 1;
+    n2 += 1;
+
+    console.log(funcion(n1, n2));
+}
+
+operar2Numeros(2, 4, function (num1, num2) {
+    return num1 * num2;
+});
+
+// For Each
