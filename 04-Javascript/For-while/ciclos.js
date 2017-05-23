c36ff941db6063906a0e359c063cdc69e75526f8
Andrés Guerra Vásconez <andreu.g.95@gmail.com>
2017-05-22 19:59:20 -0500

    Ciclos y funciones

---
+++ 04-Javascript/For-while/ciclos.js
@@ -0,0 +1,11 @@
+/**
+ * Created by poli_ on 22/5/2017.
+ */
+for (var i=0; i<10; i++) {
+    console.log(i);
+}
+
+while(i>0) {
+    console.log(i);
+    i--;
+}
---
+++ 04-Javascript/Funciones/funciones.js
@@ -0,0 +1,8 @@
+/**
+ * Created by poli_ on 22/5/2017.
+ */
+function imprimirHolaMundo () {
+    console.log("Hola mundo");
+}
+
+imprimirHolaMundo();
