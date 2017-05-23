5872d15c2d2e0d113b497a3548a463e0543ca200
Andrés Guerra Vásconez <andreu.g.95@gmail.com>
2017-05-22 20:40:06 -0500

    Manejo de objetos y arreglos

--- 04-Javascript/Variables/variables.js
+++ 04-Javascript/Variables/variables.js
@@ -41,8 +41,38 @@ var objetoComplejo = {
     fechaNacimiento: new Date(),
     mayorEdad: true,
     peso: 70.2,
-    altura: 127
+    altura: 127,
+    saltar: function () {
+        console.log('Saltar');
+    },
+    correr: function () {
+        console.log('Correr');
+    },
+    sumar: function (a, b) {
+        return a + b;
+    },
+    imprimirNombre: function () {
+        //return objetoComplejo.nombre + ' ' + objetoComplejo.apellido;
+        return this.nombre + ' ' + this.apellido;
+    }
 };
 
 objetoComplejo.altura;
 objetoComplejo.apellido;
+
+objetoComplejo.imprimirNombre();
+
+objetoComplejo.titulo = "Noob";
+
+delete objetoComplejo.nombre;
+
+var arregloStrings = ['1', '2'];
+
+arregloStrings.push('3');
+arregloStrings.pop();
+
+// ['1', '3', '2']
+arregloStrings.splice(1, 0, '3');
+
+// ['3', '2']
+arregloStrings.splice(0, 1);
