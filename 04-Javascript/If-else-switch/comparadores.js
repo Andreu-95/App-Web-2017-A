fcef00f3f06f25b2031c0dac796b323e38896291
Andrés Guerra Vásconez <andreu.g.95@gmail.com>
2017-05-22 19:46:59 -0500

    If-else

---
+++ 04-Javascript/If-else-switch/comparadores.js
@@ -0,0 +1,34 @@
+/**
+ * Created by poli_ on 22/5/2017.
+ */
+var verdad = true;
+
+if (verdad == true) {
+    // Hacemos algo si es verdad
+} else {
+    // Hacemos algo si es false
+}
+
+var numero = 5;
+
+if (numero == 5) {
+    // Hacemos algo si es 5
+} else {
+    // Hacemos algo si no es 5
+}
+
+var numero2, fecha, apuesta; // undefined
+
+if (numero2) {
+
+} else {
+
+}
+
+var otroCaso = 2;
+
+if (otroCaso) {
+
+} else {
+    
+}
