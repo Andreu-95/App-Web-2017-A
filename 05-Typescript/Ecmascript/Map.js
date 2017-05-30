1648e602de469e4007ae2e2a86fa7c25556202b9
Andrés Guerra Vásconez <andreu.g.95@gmail.com>
2017-05-29 20:42:23 -0500

    Map

--- 05-Typescript/Ecmascript/Map.js
+++ 05-Typescript/Ecmascript/Map.js
@@ -1,10 +1,8 @@
 /**
  * Created by poli_ on 29/5/2017.
  */
-
-let arreglo = [1,2,3,4,5];
-let resultado = arreglo.map((valor, indice, arreglo) => {
+var arreglo = [1, 2, 3, 4, 5];
+var resultado = arreglo.map(function (valor, indice, arreglo) {
     return valor * indice;
 });
-
 console.log(resultado);
