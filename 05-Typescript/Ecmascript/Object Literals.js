f95b07ebb6ebb5d34d7495969bea58fbf73e1627
Andrés Guerra Vásconez <andreu.g.95@gmail.com>
2017-05-29 20:20:00 -0500

    Object Literals

--- 05-Typescript/Ecmascript/Object Literals.js
+++ 05-Typescript/Ecmascript/Object Literals.js
@@ -1,15 +1,12 @@
 /**
  * Created by poli_ on 29/5/2017.
  */
-
-function multiplicarX2 (n1, n2) {
-    let resultado = {
-        n1,
-        n2
+function multiplicarX2(n1, n2) {
+    var resultado = {
+        n1: n1,
+        n2: n2
     };
-
     resultado.n1 = resultado.n1 * 2;
     resultado.n2 = resultado.n2 * 2;
-
     return resultado;
 }
