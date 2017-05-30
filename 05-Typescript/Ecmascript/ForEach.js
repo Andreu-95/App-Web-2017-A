d8527fc60bc7df729064c223c614ad36304f0cd5
Andrés Guerra Vásconez <andreu.g.95@gmail.com>
2017-05-29 20:30:37 -0500

    For Each

--- 05-Typescript/Ecmascript/ForEach.js
+++ 05-Typescript/Ecmascript/ForEach.js
@@ -1,13 +1,10 @@
 /**
  * Created by poli_ on 29/5/2017.
  */
-
-let arregloNumeros = [1,2,3,4,5];
-
-let resultado = arregloNumeros.forEach((valor, indice, arreglo) => {
+var arregloNumeros = [1, 2, 3, 4, 5];
+var resultado = arregloNumeros.forEach(function (valor, indice, arreglo) {
     console.log("El valor es " + valor);
     console.log("El índice es " + indice);
     console.log("El arreglo es " + arreglo);
 });
-
 console.log(resultado);
