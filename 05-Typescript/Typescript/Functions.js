e737e66496e2d9f69aba97c01afa44a22385ead8
Andrés Guerra Vásconez <andreu.g.95@gmail.com>
2017-06-03 08:51:26 -0500

    Functions

---
+++ 05-Typescript/Typescript/Functions.js
@@ -0,0 +1,9 @@
+/**
+ * Created by poli_ on 3/6/2017.
+ */
+function sumar2Numeros(numero1, numero2) {
+    if (numero1 === void 0) { numero1 = 12; }
+    return numero1 + numero2;
+}
+console.log(sumar2Numeros(1, 2));
+console.log(sumar2Numeros());
--- 05-Typescript/Typescript/Functions.ts
+++ 05-Typescript/Typescript/Functions.ts
@@ -1,3 +1,10 @@
 /**
  * Created by poli_ on 3/6/2017.
  */
+
+function sumar2Numeros (numero1: number = 12, numero2?: number): number {
+    return numero1 + numero2;
+}
+
+console.log(sumar2Numeros(1,2));
+console.log(sumar2Numeros());
