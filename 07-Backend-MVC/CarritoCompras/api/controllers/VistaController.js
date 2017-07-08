89430db6c5499ddf57c1cb478ede1dc054c17de3
Andrés Guerra Vásconez <andreu.g.95@gmail.com>
2017-07-08 08:09:06 -0500

    Prueba ejs

--- 07-Backend-MVC/CarritoCompras/api/controllers/VistaController.js
+++ 07-Backend-MVC/CarritoCompras/api/controllers/VistaController.js
@@ -1,5 +1,13 @@
 module.exports = {
     vistaOculta: function (req, res) {
         return res.view('Oculto/sorpresa');
+    },
+    homepage: function (req, res) {
+        var usuarioModelo = {
+            nombre: 'Andres',
+            apellido: 'Guerra',
+            id: 1
+        };
+        return res.view('homepage', { usuario: usuarioModelo });
     }
 };
--- 07-Backend-MVC/CarritoCompras/api/controllers/VistaController.ts
+++ 07-Backend-MVC/CarritoCompras/api/controllers/VistaController.ts
@@ -6,5 +6,14 @@ declare let module;
 module.exports = {
   vistaOculta: (req, res) => {
     return res.view('Oculto/sorpresa');
+  },
+  homepage: (req, res) => {
+    let usuarioModelo = {
+      nombre: 'Andres',
+      apellido: 'Guerra',
+      id: 1
+    };
+
+    return res.view('homepage', {usuario: usuarioModelo});
   }
 };
